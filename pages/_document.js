fff7a3516ed997f02359a768e09d1c559707d959
markrajk <marko88rajkovic@gmail.com>
2023-02-01 17:26:33 +0100

    README.md update

--- pages/_document.js
+++ pages/_document.js
@@ -35,7 +35,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link rel="icon" href="/images/favicon-32x32.png" />
+          <link rel="icon" href="./images/favicon-32x32.png" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&family=Raleway:wght@400;700&display=swap" rel="stylesheet" />
